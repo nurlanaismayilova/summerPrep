0f96cd2ca2e25f4a55d6b1dbdcd2f5abf53b50f7
nurlanaismayilova <150562064+nurlanaismayilova@users.noreply.github.com>
2024-06-15 23:24:22 +0400

    First Homework (Task 1,2,3)

---
+++ FirstHW/Program.cs
@@ -0,0 +1,40 @@
+﻿/*//Task 1
+Console.WriteLine("Choose 4 digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 1000 && number <= 9999)
+{
+    Console.WriteLine($"Selected Number: {number}");
+    if (number % 3 == 0)
+    {
+        Console.WriteLine("This number is divisible by 3. True");
+    } else if (number % 7 == 0) {
+        Console.WriteLine("This number is divisible by 7.True");
+    }
+    else
+    {
+        Console.WriteLine("False");
+    }
+}
+else if(number <= 1000 || number >= 9999)
+{
+    Console.WriteLine("Please, add 4 digit number.");
+}*/
+/*//Task 2
+string name = "Nurlana";
+Console.WriteLine("Enter a name: ");
+string name2 = Console.ReadLine();
+if(name.Equals(name2))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}*/
+
+//Task 3
+/*int n = Convert.ToInt32(Console.ReadLine());
+int lastDigit = n % 10;
+Console.WriteLine(lastDigit);
+*/
