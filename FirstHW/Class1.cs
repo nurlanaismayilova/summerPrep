b060f1bb39fab52d59e9b605e6f3241fa2e30804
nurlanaismayilova <150562064+nurlanaismayilova@users.noreply.github.com>
2024-06-18 21:26:33 +0530

    hw2 task 1

---
+++ FirstHW/Class1.cs
@@ -0,0 +1,27 @@
+﻿//Task 1
+using System.Runtime.Intrinsics.X86;
+
+namespace hw2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Alex", "Alice", "Bam", "Nurlana" };
+
+            Console.WriteLine(countA(names));
+        }
+        static int countA(List<string> names)
+        {
+            int cnt = 0;
+            foreach (string name in names)
+            {
+                if (name.StartsWith('A'))
+                {
+                    cnt++;
+                }
+            }
+            return cnt;
+        }
+    }
+}
